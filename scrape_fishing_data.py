f12d7143647dd31e4fe973e960a25189916463aa
cburdon <cburdon11@gmail.com>
2019-07-29 11:59:31 -0500

    Fixed get_moon_phase and added prototype weather reporter.

--- scrape_fishing_data.py
+++ scrape_fishing_data.py
@@ -3,7 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import dateparser
 import datetime
+import re
 from googlesearch import search
+today = (pd.Timestamp(datetime.date.today()))
 
 
 def get_tides():
@@ -18,7 +20,7 @@ def get_tides():
         new_date = dateparser.parse(date)
         new_date_list.append(new_date)
     new_df[0] = new_date_list
-    today = (pd.Timestamp(datetime.date.today()))
+    # today = (pd.Timestamp(datetime.date.today()))
     margin = datetime.timedelta(weeks=1)
     cutoff = today + margin
     week_df = new_df[new_df[0] < cutoff]
@@ -26,10 +28,12 @@ def get_tides():
     high_tide_df = week_df[week_df["Status"] == 'High Tide']
     low_tide_df = week_df[week_df['Status'] == 'Low Tide']
     sunrise_df = week_df[week_df['Status'] == "Sunrise"]
+    tide_df_list = [sunrise_df, low_tide_df, high_tide_df]
 
     print(sunrise_df)
     print(low_tide_df)
     print(high_tide_df)
+    return tide_df_list
 
 
 def get_weather():
@@ -40,53 +44,61 @@ def get_weather():
     new_df = df.drop(['Day'], axis=1)
     new_df.columns = ['Day', 'Description', 'High/Low', 'Precip', 'Wind', 'Humidity']
     date_list = new_df['Day']
+    #print(date_list)
     new_date_list = []
     for date in date_list:
         new_date = dateparser.parse(date)
         new_date_list.append(new_date)
-    today = (pd.Timestamp(datetime.date.today()))
+    # today = (pd.Timestamp(datetime.date.today()))
     new_date_list[0] = today
+    #print(new_date_list)
     new_df['Day'] = new_date_list
     margin = datetime.timedelta(weeks=1)
     cutoff = today + margin
     week_df = new_df[new_df['Day'] < cutoff]
     print(week_df)
+    return week_df
+
+
+def weather_conditions(week_df):
+    for ind in week_df.index:
+        print('for the day of ' + str(week_df['Day'][ind]) + ', the conditions are: ' + str(week_df['Description'][ind])
+              + ', the chance of rain is: ' + str(week_df['Precip'][ind]))
 
 
 def get_moon_phase():
-    # this is the function that isn't working properly.  It finds the url of the current month and year then
-    # parses the table of moon phases, but all I can get is the table header when I need the table data instead.
-    # Ideally I want to have it in a dataframe like the stuff in get_tides and get_weather, and then from there I want
-    # to add it to a database.  Haven't gotten to that yet.
-    month_list = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
-                  'august', 'september', 'october', 'november', 'december']
-
-    month_number = datetime.date.today().month
-    year_number = datetime.date.today().year
-    for j in range(1, 13):
-        if month_number == j:
-            month = month_list[j - 1]
-        pass
-    url = f'https://www.moongiant.com/moonphases/{month}/{year_number}/'
+    url = 'https://www.timeanddate.com/moon/phases/'
 
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
-    table = soup.find('table')  # this will only find the table header, and the below code doesn't work as it should.
-    table_rows = table.find_all('tr')
-    for tr in table_rows:
-        td = tr.find_all('td')
-        print(td)  # this is empty for some reason. I've commented out some of the code to try and break down where it fails.
-        # row = [i.text for i in td]
-        # print(row)
+    table = soup.find(id='mn-cyc')
+    table_data = table.find_all('td')
+    phase_list = []
+    date_list = []
 
-    # df = pd.read_html(url, header=0)
-    # print(df)
+    pattern = re.compile(r'(?:Third|First|New|Full)[ ](?:Quarter|Moon)')
+    matches = pattern.findall(str(table_data))
+    for match in matches:
+        phase_list.append(match)
+
+    pattern = re.compile(r'\w+[ ]\d{1,2}')
+    matches = pattern.findall(str(table_data))
+    for match in matches:
+        date_list.append(match)
+    new_date_list = []
+    for date in date_list:
+        new_date = dateparser.parse(date)
+        new_date_list.append(new_date)
+
+    moon_df = pd.DataFrame()
+    moon_df['Phase'] = phase_list
+    moon_df["Date"] = new_date_list
+    return moon_df
 
 
-# get_tides()
+get_tides()
+weather_conditions(get_weather())
 # get_weather()
-#
-#
 get_moon_phase()
 
 
