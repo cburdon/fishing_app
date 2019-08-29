9f93130f321b6c682fd73f72f4c262a3f20503bd
cburdon <cburdon11@gmail.com>
2019-08-29 12:42:19 -0500

    Updated get_moon_phase and finished prototype of score_weather.

--- scrape_fishing_data.py
+++ scrape_fishing_data.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 import dateparser
 import datetime
 import re
-from googlesearch import search
-today = (pd.Timestamp(datetime.date.today()))
+today = dateparser.parse(str(datetime.date.today()))
 
 # get_tides: loads html file of specific tide table for Pearlington, MS into a Pandas dataframe.  Cleans dataframe and
 # selects the data for the next week.
@@ -40,8 +39,13 @@ def get_tides():
 
 
 # get_weather: loads html file of url for weather data for Pearlington MS into a Pandas Dataframe. Cleans the data and
-# selects a week's worth of weather.
+# selects a week's worth of weather. UPDATE: Some of the dates are being parsed with no space between the day and month.
+# This causes the dateparser not to recognize them as dates and skip them.  Fixed using regex and removing the day
+# entirely, as dateparser doesn't need it to spit out a standardised datetime. Also removed % from precipitation list to
+# make numerical comparison easier in score_weather function.
 # return: Pandas dataframe with a week of weather data.
+
+
 def get_weather():
     response = requests.get('https://weather.com/weather/tenday/l/f795e22a7d7da0717643ec585d76482217d135add9e3c78'
                             '2015376e5cd987cd4')
@@ -51,33 +55,34 @@ def get_weather():
     new_df = df.drop(['Day'], axis=1)
     new_df.columns = ['Day', 'Description', 'High/Low', 'Precip', 'Wind', 'Humidity']
     date_list = new_df['Day']
-    #print(date_list)
+    print(date_list)
     new_date_list = []
+    fixed_date_list = []
     for date in date_list:
+        matches = re.findall(r'[A-Z]{3}[ ]\d+', date)
+        fixed_date = matches[0]
+        fixed_date_list.append(fixed_date)
+
+    for date in fixed_date_list:
         new_date = dateparser.parse(date)
         new_date_list.append(new_date)
-    # today = (pd.Timestamp(datetime.date.today()))
-    new_date_list[0] = today
-    #print(new_date_list)
     new_df['Day'] = new_date_list
     margin = datetime.timedelta(weeks=1)
     cutoff = today + margin
-    week_df = new_df[new_df['Day'] < cutoff]
-    print(week_df)
-    return week_df
-
-
-# weather_conditions: takes the get_weather dataframe as an argument and prints each day and weather conditions
-# contained within the dataframe.
-def weather_conditions(week_df):
-    for ind in week_df.index:
-        print('for the day of ' + str(week_df['Day'][ind]) + ', the conditions are: ' + str(week_df['Description'][ind])
-              + ', the chance of rain is: ' + str(week_df['Precip'][ind]))
+    week_df = new_df[new_df['Day'] <= cutoff]
+    precip_list = []
+    for i in week_df.index:
+        precip_list.append(week_df['Precip'][i].split('%')[0])
+    new_week_df = week_df.drop(['Precip'], axis=1)
+    new_week_df['Precip'] = precip_list
+    print(new_week_df)
+    return new_week_df
 
 
 # get_moon_phase: loads a url as a BeautifulSoup object.  Parses that url as a string through a regex to extract moon
 # phase data and the corresponding dates.  Converts the dates to a standard datetime format using dateparser.
 # return: a Pandas dataframe containing the moon phase data.
+
 
 def get_moon_phase():
     url = 'https://www.timeanddate.com/moon/phases/'
@@ -110,40 +115,75 @@ def get_moon_phase():
     return moon_df
 
 
-# phase_add: takes get_weather dataframe and get_moon_phase dataframe as arguments.  Iterates through them and checks if
-# any dates are within 3 days of a moon phase.  If so, that moon phase is appended to a list.  If no moon phases match a
-# given date, append "No phase data" to the list.  NOTE: iteration is still not working as intended.  The conditional
-# statements do not seem to catch the days that come before a given moon phase, and don't reliably flag the right moon
-# phase.  Also occasionally they append more than 7 items to the list of phases, so sometimes the flag variable doesn't
-# prevent multiple phase statuses from being appended for the same date. Work in Progress.
-def phase_add(moon_df, week_df):
-    phases = []
-    margin = datetime.timedelta(days=3)
-    for ind in moon_df.index:
-        flag = True
-        phase_day = moon_df['Day'][ind]
-        phase_status = moon_df['Phase'][ind]
-        for i in week_df.index:
-            upper_limit = phase_day + margin
-            lower_limit = phase_day - margin
-            if lower_limit <= week_df['Day'][i] <= upper_limit:
-                phases.append(phase_status)
-                flag = False
+# phase_add_v2: takes get_weather dataframe, get_moon_phase dataframe, and an int for days margin as arguments.
+# Iterates through them and checks if any dates are within 3 days of a moon phase.  If so, that moon phase is appended
+# to a list.  If no moon phases match a given date, append "No phase data" to the list.
+# return: a new dataframe with Phase Status added as a column
+
+
+def phase_add_v2(moon_df: pd.DataFrame, week_df: pd.DataFrame, days_margin=3):
+    rtn = {}
+    margin = datetime.timedelta(days=days_margin)
+    phases = {}
+    for idx in moon_df.index:
+        phases[moon_df['Phase'][idx]] = moon_df['Day'][idx]
+    for idx in week_df.index:
+        day = week_df['Day'][idx]
+        # print(idx, day)
+        for phase in phases:
+            if day - margin <= phases[phase] <= day + margin:
+                rtn[day] = phase
+                break
             else:
-                pass
-        if flag:
-            phases.append('No phase data')
+                try:
+                    ph = rtn.pop(day)
+                    rtn[day] = ph
+                except KeyError:
+                    rtn[day] = 'No phase'
+    phase_column = []
+    for p in rtn:
+        phase_column.append(rtn[p])
+    new_moon_df = week_df
+    new_moon_df['Phase Status'] = phase_column
+    print(new_moon_df)
+    return new_moon_df
 
-# phases.append('No phase data')
-    print(phases)
 
-# get_tides()
-# weather_conditions(get_weather())
+# score_weather: takes 2 dataframes as arguments. Checks if weather condition values are preferrable for each row of the
+# first dataframe and changes a score value accordingly. Adds a score column to the first dataframe.  NOTE: Work in
+# progress.  Second dataframe is proving difficult since it is a list of dataframes and not all days have high tide and
+# some have multiple values.  It will not be as easy as checking the other values was.
+
+
+def score_weather(new_moon_df, tide_df_list):
+    scores = []
+    for i in new_moon_df['Day'].index:
+        scores.append(0)
+    for i in new_moon_df.index:
+
+        if int(new_moon_df["Precip"][i]) <= 50:
+            scores[i] = scores[i] + 5
+        if int(new_moon_df["Precip"][i]) <= 30:
+            scores[i] = scores[i] + 2
+        if new_moon_df['Phase Status'][i] == 'First Quarter':
+            scores[i] = scores[i] + 5
+        if new_moon_df['Phase Status'][i] == 'Third Quarter':
+            scores[i] = scores[i] + 5
+        if new_moon_df['Phase Status'][i] == 'New Moon':
+            scores[i] = scores[i] + 10
+        if int(new_moon_df['Wind'][i].split(' ')[1]) <= 15:
+            scores[i] = scores[i] + 2
+        else:
+            pass
+    new_moon_df['Score'] = scores
+    high_score = new_moon_df.loc[new_moon_df['Score'].idxmax()]
+    print(new_moon_df)
+    print(high_score)
+
+
 # get_weather()
-# get_moon_phase()
-
-
-phase_add(get_moon_phase(), get_weather())
-
-
+# phase_add(get_moon_phase(), get_weather())
+# phase_add_v2(moon_df=get_moon_phase(), week_df=get_weather())
+# phase_add_v2(get_moon_phase(), get_weather())
+score_weather(phase_add_v2(get_moon_phase(), get_weather()), get_tides())
 
